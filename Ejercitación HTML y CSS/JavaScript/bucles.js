656cca7a14cdbc5cf8fea47ca0227496122dd2b5
Deybi2 <deybijuaniquina@gmail.com>
2024-04-22 16:55:45 -0300

    Pokemon

---
+++ Ejercitación HTML y CSS/JavaScript/bucles.js
@@ -0,0 +1,23 @@
+//Bucles y arrays
+
+const array = [2,3,"pepe"];
+
+//while(del ingles mientras)
+while(array.length < 1){
+  console.log("test");
+}
+
+//Bucle for 
+for(let i = 1; i < array.length; i++){
+    console.log(i)
+    //Siempre adentro del for van a usar let
+    console.log("posicion", i, "valor",array[i])
+}
+
+ya no se que Hacer
+ya no se que Hacer
+ya no se que Hacer
+ya no se que Hacer
+ya no se que Hacer
+ya no se que Hacer
+ya no se q hacerer
---
+++ Ejercitación HTML y CSS/JavaScript/variables.js
@@ -0,0 +1,23 @@
+
+var numero = 1; /*asignamos el valor a una variable tipo number*/
+var palabra = 'palabra'/*asignamos el valor a una variable tipo string*/
+
+//Definimos variables con la palabra reservado let para evitar el llamado de las mismas desde cualquier parte del codigo
+
+let numero2 = 2;
+let palabra2 = 'palabra 2';
+
+//Definimos constantes cp la palabra reservada const
+
+const numConstante = 4//Siempre que sepan que no van a mutar o modificar 
+
+//ejemplo:
+
+if (numero > 0) {
+    let numero3 =3;
+}
+
+console.log("numero", numero2);//Mostrar un valor por pantalla
+
+
+//////////////////////////////////////////////////////////////////
