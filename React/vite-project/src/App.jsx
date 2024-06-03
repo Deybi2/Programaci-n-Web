cc85d8a5b2964d4afcc8339b0bcf6175ff946848
Deybi2 <deybijuaniquina@gmail.com>
2024-06-03 16:56:01 -0300

    React

---
+++ React/vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+import HolaMundo from "./components/Mi primer componente/MiPrimerComponente";
+export default function App() {
+
+  return (
+    <div>
+      <h1>Bienvenido a mi aplicación</h1>
+        <HolaMundo />
+    </div>
+  );
+}
+
---
+++ React/vite-project/src/components/Mi primer componente/MiPrimerComponente.jsx
@@ -0,0 +1,10 @@
+function HolaMundo() {
+    return (
+      <>
+      Hola Mundo
+      <img src="./imgs/sprouts-in-the-shape-of-text-imagen.webp" alt="" />
+      </>
+    );
+  }
+
+export default HolaMundo;
